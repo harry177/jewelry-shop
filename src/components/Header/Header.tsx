493ad397454dcb6beff61781f25fce6858b5cff9
Harry177 <harry177@rambler.ru>
2024-02-12 03:18:02 +0100

    feat: add burger icon and basic implementation of burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 import { AiOutlineUser, AiOutlineHeart, AiOutlineShoppingCart } from "react-icons/ai";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { GeneralDropdown } from "@/components/UI/GeneralDropdown";
 
 const menuItems = [
@@ -8,19 +12,74 @@ const menuItems = [
   { title: "Certificates", subItems: ["raz", "dva", "tri"] },
 ];
 
+const genericHamburgerLine = `h-1 w-6 my-1 rounded-full bg-black transition ease transform duration-300`;
+
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const isMobile = useMediaQuery("(max-width: 767px)");
   return (
     <header className="flex justify-around items-center h-20 shadow-md">
-      <div className="text-3xl font-semibold">J-Shop</div>
-      <nav className="flex justify-center">
-        <ul className="flex justify-between gap-[12vw]">
-          {menuItems.map((item, index) => (
-            <li key={index} className="group relative flex ">
-              <GeneralDropdown triggerName={item.title} daMenuPoints={item.subItems} />
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <div className="flex justify-between items-center gap-5">
+        {isMobile && (
+          <>
+            <button
+              className="flex flex-col h-12 w-12 border-2 border-black rounded justify-center items-center group"
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              <div
+                className={`${genericHamburgerLine} ${
+                  isOpen
+                    ? "rotate-45 translate-y-3 opacity-50 group-hover:opacity-100"
+                    : "opacity-50 group-hover:opacity-100"
+                }`}
+              />
+              <div
+                className={`${genericHamburgerLine} ${isOpen ? "opacity-0" : "opacity-50 group-hover:opacity-100"}`}
+              />
+              <div
+                className={`${genericHamburgerLine} ${
+                  isOpen
+                    ? "-rotate-45 -translate-y-3 opacity-50 group-hover:opacity-100"
+                    : "opacity-50 group-hover:opacity-100"
+                }`}
+              />
+            </button>
+            <div
+              className={`absolute flex w-80 min-h-60 top-20 ${isOpen ? "left-0" : "-left-80"} shadow-md rounded-br-lg transition-all duration-300`}
+            >
+              <nav className="flex justify-center pt-2 ml-[94px]">
+                <ul className="flex flex-col gap-5 items-start justify-evenly w-full">
+                  {menuItems.map((item, index) => (
+                    <li key={index} className={`${index === 1 && "top-[-2px]"} flex relative`}>
+                      <GeneralDropdown
+                        triggerVariant="burger"
+                        contentVariant="burger"
+                        itemVariant="burger"
+                        triggerName={item.title}
+                        daMenuPoints={item.subItems}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </nav>
+            </div>
+          </>
+        )}
+
+        <span className="text-3xl font-semibold">J-Shop</span>
+      </div>
+      {!isMobile && (
+        <nav className="flex justify-center">
+          <ul className="flex justify-between gap-[7vw] lg:gap-[12vw]">
+            {menuItems.map((item, index) => (
+              <li key={index} className="group relative flex ">
+                <GeneralDropdown triggerName={item.title} daMenuPoints={item.subItems} />
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
       <div className="w-32 h-10 flex justify-around items-center">
         <Link href="/">
           <AiOutlineUser className="w-5 h-5" />
--- src/components/UI/GeneralDropdown.tsx
+++ src/components/UI/GeneralDropdown.tsx
@@ -5,11 +5,13 @@ import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/shared/utils";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import "./ui-styles.css";
 
 const triggerVariants = cva("flex items-center justify-between outline-none cursor-pointer", {
   variants: {
     variant: {
-      default: "gap-5 font-semibold lg:font-medium",
+      default: "gap-5 font-semibold text-sm lg:text-base font-medium",
+      burger: "",
     },
   },
   defaultVariants: {
@@ -21,6 +23,7 @@ const contentVariants = cva("bg-[#ffffff] font-normal", {
   variants: {
     variant: {
       default: "w-20 shadow-md",
+      burger: "relative w-[200px]",
     },
   },
   defaultVariants: {
@@ -32,6 +35,7 @@ const itemVariants = cva("inline-flex items-center text-[13px] px-[5px] rounded-
   variants: {
     variant: {
       default: "pl-[10px] mb-[10px] mr-[20px] lg:text-[10px] lgb:text-[13px]",
+      burger: "w-[120px] pl-[10px] mb-[10px] mr-[20px]",
     },
   },
   defaultVariants: {
@@ -57,27 +61,32 @@ export const GeneralDropdown = ({
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <DropdownMenu.Root open={isOpen} onOpenChange={setIsOpen}>
-      <DropdownMenu.Trigger
-        asChild
-        style={{ pointerEvents: "auto" }}
-        onMouseEnter={() => setIsOpen(true)}
-        onMouseLeave={() => setIsOpen(false)}
-      >
-        <div className={cn(triggerVariants({ variant: triggerVariant }))}>
-          {triggerName}
-          {isOpen ? <AiOutlineUp /> : <AiOutlineDown />}
-        </div>
-      </DropdownMenu.Trigger>
-      <DropdownMenu.Content
-        className={cn(contentVariants({ variant: contentVariant }))}
-        onMouseEnter={() => setIsOpen(true)}
-        onMouseLeave={() => setIsOpen(false)}
-      >
-        {daMenuPoints.map((menuPoint, index) => (
-          <DropdownMenu.Item key={index}>{menuPoint}</DropdownMenu.Item>
-        ))}
-      </DropdownMenu.Content>
-    </DropdownMenu.Root>
+    <span className={`${triggerVariant === "burger" ? "SuperContent" : ""}`}>
+      <DropdownMenu.Root open={isOpen} onOpenChange={setIsOpen}>
+        <DropdownMenu.Trigger
+          asChild
+          style={{ pointerEvents: "auto" }}
+          onMouseEnter={() => triggerVariant === "default" && setIsOpen(true)}
+          onMouseLeave={() => triggerVariant === "default" && setIsOpen(false)}
+          onPointerDown={() => triggerVariant !== "default" && setIsOpen((prev) => !prev)}
+        >
+          <div className={cn(triggerVariants({ variant: triggerVariant }))}>
+            {triggerName}
+            {isOpen ? <AiOutlineUp /> : <AiOutlineDown />}
+          </div>
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content
+          className={cn(contentVariants({ variant: contentVariant }))}
+          onMouseEnter={() => triggerVariant === "default" && setIsOpen(true)}
+          onMouseLeave={() => triggerVariant === "default" && setIsOpen(false)}
+        >
+          {daMenuPoints.map((menuPoint, index) => (
+            <DropdownMenu.Item key={index} className={cn(itemVariants({ variant: itemVariant }))}>
+              {menuPoint}
+            </DropdownMenu.Item>
+          ))}
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </span>
   );
 };
